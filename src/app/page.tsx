329cc37b6b7eb1fc69f57528793578ae038b8531
rohitDalalStrique <rohit.dalal@strique.io>
2024-11-07 20:01:33 +0530

    manual captureException page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,18 @@
+import { captureException } from "@sentry/nextjs";
 import Image from "next/image";
 
 export default async function Home() {
   fetch('https://jsonplaceholder.typicode.com/todos/')
     .then(response =>  {
-      if(response.ok) throw new Error("Testing error from fetch.then in page.tsx")
+      if(response.ok)  {
+        captureException("Testing error from fetch.then in page.tsx");
+        throw new Error("Testing error from fetch.then in page.tsx")
+      }
       return response.json()
     })
     .then(json => console.log(json))
 
-    
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">

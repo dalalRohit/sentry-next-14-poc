14e742c682687056ab9b13f54bac988c3bc089d8
rohitDalalStrique <rohit.dalal@strique.io>
2024-11-07 19:09:10 +0530

    add GET in layout with throw Error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -35,7 +35,13 @@ export default function RootLayout({
     Sentry.setUser({id:userId,email:""})
   }
 
-  throw new Error("Error from layout");
+  fetch('https://jsonplaceholder.typicode.com/todos/')
+      .then(response =>  {
+        if(response.ok) throw new Error("Testing error from fetch.then")
+        return response.json()
+      })
+      .then(json => console.log(json))
+
   return (
     <html lang="en">
       <body
